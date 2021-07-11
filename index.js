d8510e0b041cc6cc452178750e7169d1fd42dea3
Yiyang Huang <oliviahyy1126@gmail.com>
2021-07-11 20:56:14 +1000

    add unknownEndpoint error handling to exercise 3.10 solution

--- index.js
+++ index.js
@@ -97,7 +97,13 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
